13614f2faad7e5a7dfa0ddd37aa3d3ef7c364807
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-08-22 19:23:48 +0300

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,31 +10,40 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        return self.__objects
+    def all(self, cls=None):
+        """Returns the list of objects of one type of class."""
+        if cls is None:
+            return self.__objects
+        return {key: obj for key, obj in self.__objects.items() if isinstance(obj, cls)}
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
         if obj:
-            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
         obj_dict = {}
-
         for key, obj in self.__objects.items():
             obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as f:
+        with open(self.__file_path, "w") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """Deserialize/convert obj dicts back to instances, if it exists"""
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(self.__file_path, "r") as f:
                 new_obj_dict = json.load(f)
             for value in new_obj_dict.values():
-                class_Name = value["__class__"]
-                self.new(eval(class_Name)(**value))
+                class_name = value["__class__"]
+                self.new(eval(class_name)(**value))
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside."""
+        if obj:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
