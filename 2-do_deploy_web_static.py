ab12696dcfc7977397c82b2a7b91e2e9e9fe6e02
0marSalah <omar.salah1597@gmail.com>
2023-09-07 18:53:55 +0300

    واحد اسمه محمد وواحد اسمه عيد

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.87.228.212	', '54.236.50.147']
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
@@ -14,17 +14,17 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        file_name = archive_path.split("/")[-1]
-        file_x_ext = file_name.split(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_x_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_x_ext))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_x_ext))
-        run('rm -rf {}{}/web_static'.format(path, file_x_ext))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_x_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-Fabric script that creates and distributes an archive to the web servers
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
 """
 
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.87.228.212	', '54.236.50.147']
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_pack():
